ee5d3a2ccedffc11283fd1cad3ba4952a98e3762
StefanoCodes <stefanovidmarbusiness@gmail.com>
2024-11-23 22:22:49 +0200

    about us heading text animation

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,22 +2,29 @@ import KalonTag from "@/components/kalon-tag";
 import content from "./about.config";
 import Hr from "@/components/hr";
 import Image from "next/image";
+import { WordFadeIn } from "@/components/motion/text-slide";
+import { FadeText } from "@/components/motion/text-fade";
+import { delay } from "motion";
 const { title } = content;
 export default function About() {
   return (
     <main className="py-24">
       <section id="about" className="container">
         <div className="flex flex-col gap-16">
-
-          {/* FIRST PART OF THE CONTENT */}
+          {/* HEADER*/}
           <div className="flex flex-col gap-16">
             <div>
-
-              <h1 className="text-4xl font-medium leading-[1.5] tracking-tight text-whiteColor md:text-5xl lg:text-8xl">
-                {title.main}
-                <br />
-                {title.sub}
-              </h1>
+              <WordFadeIn
+                words={title.main}
+                delay={0.15}
+                className="text-4xl font-medium leading-[1.5] tracking-tight text-whiteColor md:text-5xl lg:text-8xl"
+              />
+              <br />
+              <WordFadeIn
+                words={title.sub}
+                delay={0.4}
+                className="text-4xl font-medium leading-[1.5] tracking-tight text-whiteColor md:text-5xl lg:text-8xl"
+              />
             </div>
             <div className="flex flex-col gap-8">
               <div className="flex flex-col gap-4">
@@ -25,6 +32,13 @@ export default function About() {
                   We have all had moments of doubt, standing in front of the
                   mirror, wondering if we have what it takes. The truth? You do.
                 </p>
+                {/* <FadeText
+                  text="We have all had moments of doubt, standing in front of the
+                  mirror, wondering if we have what it takes. The truth? You do."
+                  direction="up"
+                  className="body-text"
+                  animationDelay={0.4}
+                /> */}
                 <p className="body-text">
                   At <KalonTag text="Kalon" />, we see more than just potential,
                   we see people ready to redefine beauty on their own terms. This
@@ -50,52 +64,57 @@ export default function About() {
             <Hr />
           </div>
 
-          {/* SECOND PART OF THE CONTENT */}
-          <div className="flex flex-col gap-16">
-            {/* FIRST PART OF THE SECOND PART */}
+          {/* MAIN */}
+          <div className="flex flex-col gap-32">
+            {/* CONTENT */}
             <div className="flex flex-col gap-8">
               {/* SERVICES */}
               <div className=" flex flex-col gap-16">
                 <div className="flex items-center justify-between">
                   <div>
-                    <h3>Services</h3>
+                    <h2>Services</h2>
                   </div>
                   <div>
-                    <h3>Services</h3>
+                    <h2>Services</h2>
                   </div>
                   <div>
-                    <h3>Services</h3>
+                    <h2>Services</h2>
                   </div>
                 </div>
                 <Hr gradient="dark" />
               </div>
               {/* AUTOBIOGRAPHY */}
               <div className="flex items-start justify-between gap-16">
-                <Image
-                  src={'/about/kalon_placeholder.jpg'}
-                  alt="Kalon"
-                  width={400}
-                  height={400}
-                  className=" object-cover"
-                />
+                <div className="flex-1">
+                  <Image
+                    src={'/about/kalon_placeholder.jpg'}
+                    alt="Kalon"
+                    width={304}
+                    height={304}
+                    className=" max-w-md h-full aspect-square object-cover"
+                  />
+                </div>
 
                 <div>
                   {/* CONTENT */}
-                  <div className="flex flex-col gap-5">
-                    <p className="body-text">
-                      My modeling journey began in 2017, but my career truly started in 2020. The first three years were particularly challenging—not due to rejection or missed opportunities, but because I didn’t receive the proper guidance.
-                    </p>
+                  <div className="flex flex-col gap-8">
+                    <div className="flex flex-col gap-5 max-w-xl">
 
-                    <p className="body-text">
-                      This led me to establishing Kalon Models in 2020, from there on out I made it my mission to create a supportive environment where models can thrive, ensuring that no one has to navigate the industry alone.
-                    </p>
 
-                    <div>
+                      <p className="body-text">
+                        My modeling journey began in 2017, but my career truly started in 2020. The first three years were particularly challenging—not due to rejection or missed opportunities, but because I didn’t receive the proper guidance.
+                      </p>
+
+                      <p className="body-text">
+                        This led me to establishing Kalon Models in 2020, from there on out I made it my mission to create a supportive environment where models can thrive, ensuring that no one has to navigate the industry alone.
+                      </p>
+                    </div>
+                    <div className="flex flex-col gap-2">
                       <Image
                         src={'/about/signature.svg'}
                         alt="Kalon"
-                        width={400}
-                        height={400}
+                        width={80}
+                        height={36}
                         className=" object-cover"
                       />
                       <p className="body-text">
@@ -107,8 +126,16 @@ export default function About() {
                 </div>
               </div>
             </div>
-            {/* BOTTOM CONTENT OF THE SECOND PART */}
+            {/* FOOTER */}
+            <div className="flex flex-col items-center justify-center gap-8">
+              <h3 className="body-text text-2xl">Our approach is simple</h3>
+              <p className="body-text text-2xl text-center">
+                We exist to shape top-tier models with unmatched skill and confidence.  We see potential AND REFINE IT. Welcoming every background, we find beauty in stories, strength, and the confidence we help build. Whether it’s your first step on the runway or your hundredth, this is where you discover your voice, your power, and your place.
+
+              </p>
+            </div>
           </div>
+
         </div>
       </section>
     </main>
