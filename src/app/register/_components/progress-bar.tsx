e3d6b9d1ef0f9f71d31e876d9b585e7a3d3357a6
StefanoCodes <stefanovidmarbusiness@gmail.com>
2024-11-24 15:05:43 +0200

    WIP Registartion Form

--- src/app/register/_components/progress-bar.tsx
+++ src/app/register/_components/progress-bar.tsx
@@ -1,5 +1,27 @@
-import DisplaySteps from "./steps";
+import { cn } from "@/lib/utils"
 
-export default function ProgressBar() {
-    return <DisplaySteps />
+export default function ProgressBar({ progressPercentage, className }: {
+    progressPercentage?: 33 | 66 | 100,
+    className?: string,
+}) {
+    return (
+        <div className="w-full bg-gray-200 rounded-full h-1 dark:bg-gray-700">
+            <div className={cn("bg-blue-600 h-1 rounded-full", className)} style={{ width: `${progressPercentage}%` }}></div>
+        </div>
+    )
 }
+
+// export default function ProgressBar({ progressPercentage}: {
+//     progessPercentage: number
+// } {
+//     return (
+//         <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+//             <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: `${progressPercentage[progressPercentage]}%` }}></div>
+//         </div>
+
+//     )
+// })
+
+
+
+
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -18,13 +18,13 @@ export default function Register() {
                     <WordFadeIn words="Registration" delay={0.4} className="text-3xl md:text-4xl font-semibold lg:text-5xl" />
                     <FadeText text="This is a Multi-Part Form Demo! In this demonstration, we will showcase the powerful new forms tools available." className="body-text text-mutedColor" />
                 </div>
-                <section className="overflow-hidden rounded-2xl bg-white shadow-xl transition-all duration-300 ease-in-out hover:shadow-2xl" id="register">
+                <section className="overflow-hidden  rounded-2xl bg-white shadow-xl transition-all duration-300 ease-in-out hover:shadow-2xl" id="register">
                     <div className="flex flex-col-reverse md:flex-row">
-                        <div className="flex flex-col flex-[1.5] gap-4 p-4">
-                            <div className="border-b-2 border-b-orange-500">
-                                <ProgressBar />
+                        <div className="flex flex-col flex-[1.5] gap-4">
+                            <div className="">
+                                <ProgressBar progressPercentage={33} className="bg-primaryColor" />
                             </div>
-                            <div className="text-black flex flex-col gap-4">
+                            <div className="text-black flex flex-col gap-4 p-4">
                                 <p>CONTENT</p>
                                 <p>CONTENT</p>
                                 <p>CONTENT</p>
