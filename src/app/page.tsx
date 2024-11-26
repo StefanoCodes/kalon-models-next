779d6c0855f1198cebe214859c76658fdf36848f
StefanoCodes <stefanovidmarbusiness@gmail.com>
2024-11-26 19:28:15 +0200

    cleaned up the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,7 @@ export default function Home() {
   return (
     <main className="w-full px-5 py-10 md:px-10">
       <section id="home" className="container">
-        <div className="flex flex-col gap-4">
-          <Heading headingSize="h1">Home</Heading>
-          <div className="flex items-center gap-4">
-            <Link href={"/about"}>About</Link>
-            <Link href={"/register"}>Register</Link>
-          </div>
-        </div>
+        HOME
       </section>
     </main>
   );
