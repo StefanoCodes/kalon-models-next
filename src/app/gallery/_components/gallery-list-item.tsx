59f5e25a7e2b01f2c768d5b6d807697a008946f8
StefanoCodes <stefanovidmarbusiness@gmail.com>
2024-11-30 02:15:12 +0200

    small change to the gallery item font

--- src/app/gallery/_components/gallery-list-item.tsx
+++ src/app/gallery/_components/gallery-list-item.tsx
@@ -14,7 +14,7 @@ export default function GalleryListItem({
     <Link href={`/gallery/${slug}`}>
       <div className="flex flex-row justify-between gap-4 border-b border-border pb-4">
         <div className="flex flex-row items-center gap-2">
-          <h3 className="text-2xl font-medium">{title}</h3>
+          <h3 className="text-2xl font-normal">{title}</h3>
           <ArrowUpRightIcon className={`h-4 w-4`} />
         </div>
         <p className="text-sm text-gray-500">{timeline}</p>
