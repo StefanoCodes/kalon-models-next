a3fc6f9d65dce03e4198fd47a6ac044d8ee7d81b
StefanoCodes <stefanovidmarbusiness@gmail.com>
2024-11-30 11:43:30 +0200

    changed the border radius

--- src/app/gallery/[slug]/_components/sector-badge.tsx
+++ src/app/gallery/[slug]/_components/sector-badge.tsx
@@ -2,7 +2,7 @@ import { Badge } from "@/components/ui/badge";
 
 export default function GallerySlugSectorBadge({ sector }: { sector: string }) {
   return (
-    <Badge className="border border-muted-foreground bg-transparent font-normal text-paragraph hover:bg-blackColor hover:text-whiteColor">
+    <Badge className="rounded-sm border border-muted-foreground bg-transparent font-normal text-paragraph hover:bg-blackColor hover:text-whiteColor">
       {sector}
     </Badge>
   );
