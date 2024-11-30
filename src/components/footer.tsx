003f43ea4eda28d44773a7c9b6e0df657e80d08a
StefanoCodes <stefanovidmarbusiness@gmail.com>
2024-12-01 00:32:41 +0200

    social link added

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,12 @@
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { InstagramIcon } from "lucide-react";
+import { navbar } from "./navbar/navbar.config";
+
+const { socials } = navbar;
 export default function Footer() {
   const foundedYear = 2020;
+  const instagramLink = socials[0].link;
   return (
     <footer className="bg-backgroundLightAltColor">
       <div className="container flex items-center justify-between px-4 py-4">
@@ -10,7 +14,7 @@ export default function Footer() {
           Creating Experiences Since {foundedYear}
         </span>
 
-        <Link href="https://www.instagram.com/sentri.co.za/" target="_blank">
+        <Link href={instagramLink} target="_blank">
           <InstagramIcon className="h-4 w-4 text-mutedColor" />
         </Link>
       </div>
