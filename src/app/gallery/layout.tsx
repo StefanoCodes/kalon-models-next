d72153ecdb35fec7fb8d26491ece0bd9faa7085b
StefanoCodes <stefanovidmarbusiness@gmail.com>
2024-12-02 01:37:31 +0200

    updated the metadata for the gallery layout to have the same metadata

--- src/app/gallery/layout.tsx
+++ src/app/gallery/layout.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Gallery | A sneak peak into our world",
+  description: "Explore our gallery.",
+};
+
 export default function GalleryLayout({
   children,
 }: {
