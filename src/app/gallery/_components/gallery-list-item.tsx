d90d34e359dfe44e4fa55dfe417b4a9bc16609eb
StefanoCodes <stefanovidmarbusiness@gmail.com>
2024-12-02 17:54:11 +0200

    updated the gallery-list item to have an external link icon

--- src/app/gallery/_components/gallery-list-item.tsx
+++ src/app/gallery/_components/gallery-list-item.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import ExternalLink from "@/components/external-link";
 export default function GalleryListItem({
   title,
   timeline,
@@ -18,26 +19,37 @@ export default function GalleryListItem({
 }) {
   return (
     <div className={cn(`group`, className)}>
-      <Link href={`/gallery/${slug}`} className="flex flex-col gap-4">
-        <div className="flex w-full flex-col gap-4">
+      <div className="flex flex-col gap-4">
+        {/* IMAGE */}
+        <div className="relative flex w-full flex-col gap-4">
           <Image
             src={coverImage}
             width={1000}
             height={1000}
-            className="aspect-square min-h-[450px] w-full rounded-md object-cover brightness-75 filter lg:max-h-[550px]"
+            className="aspect-square min-h-[450px] w-full rounded-md object-cover brightness-50 filter lg:max-h-[550px]"
             alt="placeholder"
           />
+          <ExternalLink
+            href={`/gallery/${slug}`}
+            className="absolute right-3 top-3 z-10 self-start text-backgroundLightAltColor transition-colors hover:text-grayLightColor"
+            iconClassName="size-4"
+            text="View More"
+            textClassName="text-sm font-normal tracking-wide md:text-[15px]"
+            reverse
+          />
         </div>
-        <div className="flex flex-col gap-4 border-b border-border pb-4 transition-all duration-300 group-hover:border-blackColor">
+        {/* CONTENT */}
+        <div className="flex flex-col gap-4 pb-4 transition-all duration-300">
           <div className="flex flex-col gap-1">
             <h2 className="text-lg font-normal md:text-xl">{title}</h2>
             <p className="text-sm text-gray-500">{timeline}</p>
           </div>
+
           <div>
             <p className="slug-content">{overview}</p>
           </div>
         </div>
-      </Link>
+      </div>
     </div>
   );
 }
--- src/components/external-link.tsx
+++ src/components/external-link.tsx
@@ -4,13 +4,32 @@ import Link from "next/link";
 export default function ExternalLink({
   href,
   className,
+  iconClassName,
+  text,
+  textClassName,
+  reverse,
 }: {
   href: string;
   className?: string;
+  iconClassName?: string;
+  text?: string;
+  textClassName?: string;
+  reverse?: boolean;
 }) {
   return (
-    <Link href={href} className={cn(className)}>
-      <ExternalLinkIcon className="size-4" />
+    <Link
+      href={href}
+      className={cn(
+        `cursor-pointer transition-colors hover:text-primaryColor`,
+        className,
+      )}
+    >
+      <div
+        className={cn("flex items-center gap-1", reverse && "flex-row-reverse")}
+      >
+        <ExternalLinkIcon className={cn("size-4", iconClassName)} />
+        {text && <span className={cn(textClassName)}>{text}</span>}
+      </div>
     </Link>
   );
 }
