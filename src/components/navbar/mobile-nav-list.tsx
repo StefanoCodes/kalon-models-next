c0d6e91ee27e98e50ecda55521dcc3ff0b33af10
StefanoCodes <stefanovidmarbusiness@gmail.com>
2024-12-04 17:42:58 +0200

    quick update to the navbar

--- src/components/navbar/mobile-nav-list.tsx
+++ src/components/navbar/mobile-nav-list.tsx
@@ -19,7 +19,7 @@ export default function MobileNavList() {
   const toggleNavigation = () => toggleOpen();
   return (
     <>
-      <div className="z-30 flex w-full items-center justify-between md:hidden">
+      <div className="z-30 flex w-full items-center justify-between sm:hidden">
         <NavLogo
           onClick={() => {
             return isOpen && toggleNavigation();
