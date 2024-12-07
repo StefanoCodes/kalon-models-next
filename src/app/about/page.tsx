bd308e8b066fe8bdfb6377d7bc7901d6df3a77a6
StefanoCodes <stefanovidmarbusiness@gmail.com>
2024-12-07 15:21:55 +0200

    updated the about us page with a cover image

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,6 +4,7 @@ import StatsList from "./_components/stats-list";
 import FounderContent from "./_components/founder-content";
 import Header from "./_components/header";
 import LatestWork from "./_components/latest-work";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "About | A Modeling Academy for Everyone, but Not Just Anyone.",
@@ -13,21 +14,33 @@ export const metadata: Metadata = {
 
 export default function About() {
   return (
-    <section
-      id="about"
-      className="container min-h-screen-mobile py-10 md:min-h-screen-desktop xl:px-0"
-    >
-      <div className="flex flex-col gap-16">
-        <Header />
-        <div className="flex flex-col gap-16">
-          <div className="flex flex-col gap-8 md:gap-12">
-            <StatsList />
-            <ServicesList />
-          </div>
-          <FounderContent />
-          <LatestWork />
-        </div>
+    <>
+      <div className="relative h-[500px] w-full">
+        <Image
+          src="/kalon-cover-home.jpg"
+          alt="About Hero"
+          fill
+          className="mx-auto max-w-[1920px] object-cover"
+          quality={100}
+          priority
+        />
       </div>
-    </section>
+      <section
+        id="about"
+        className="container min-h-screen-mobile py-10 md:min-h-screen-desktop xl:px-0"
+      >
+        <div className="flex flex-col gap-16">
+          <Header />
+          <div className="flex flex-col gap-16">
+            <div className="flex flex-col gap-8 md:gap-12">
+              <StatsList />
+              <ServicesList />
+            </div>
+            <FounderContent />
+            <LatestWork />
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
