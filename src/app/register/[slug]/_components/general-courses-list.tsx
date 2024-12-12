bdcbd26d14e59205408a6eb25c2543af4961e7b6
StefanoCodes <stefanovidmarbusiness@gmail.com>
2024-12-13 00:51:35 +0200

    updated

--- src/app/register/[slug]/_components/general-courses-list.tsx
+++ src/app/register/[slug]/_components/general-courses-list.tsx
@@ -1,19 +1,19 @@
-import { courses } from "../../register.config";
-import GeneralCourseCard from "./general-course-card";
+// import { courses } from "../../register.config";
+// import GeneralCourseCard from "./general-course-card";
 
-const { masterclass } = courses;
-export default function GeneralCoursesList() {
-  return (
-    <div className="grid gap-8 lg:grid-cols-2 lg:gap-16">
-      {masterclass.map((course) => {
-        const courseDetails = course.masterclass;
-        return (
-          <GeneralCourseCard
-            key={courseDetails.heading}
-            courseDetails={courseDetails}
-          />
-        );
-      })}
-    </div>
-  );
-}
+// const { masterclass } = courses;
+// export default function GeneralCoursesList() {
+//   return (
+//     <div className="grid gap-8 lg:grid-cols-2 lg:gap-16">
+//       {masterclass.map((course) => {
+//         const courseDetails = course.masterclass;
+//         return (
+//           <GeneralCourseCard
+//             key={courseDetails.heading}
+//             courseDetails={courseDetails}
+//           />
+//         );
+//       })}
+//     </div>
+//   );
+// }
