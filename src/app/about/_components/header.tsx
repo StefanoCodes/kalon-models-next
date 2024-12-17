26c6c476c6d2e022e2ce3d1d254de4601e9fe299
StefanoCodes <stefanovidmarbusiness@gmail.com>
2024-12-17 12:05:32 +0200

    first part of the about page is done

--- src/app/about/_components/header.tsx
+++ src/app/about/_components/header.tsx
@@ -1,47 +1,65 @@
 import { WordFadeIn } from "@/components/motion/text-slide";
 import TextRevealByWord from "./text-reveal";
-const headerAnimationStyle = `text-3xl font-medium leading-[1.1] tracking-tight text-blackColor sm:text-3xl md:text-5xl lg:text-5xl xl:text-6xl xl:leading-[1.1] text-center`;
-const headerAnimationStyleSoft = `text-3xl font-normal leading-[1.1] tracking-tight text-blackColor sm:text-3xl md:text-5xl lg:text-5xl xl:text-6xl xl:leading-[1.1] text-center`;
+import Hr from "@/components/hr";
+const gridCols = {
+  1: "col-span-12 md:col-span-4 xl:col-span-2",
+  2: "col-span-12 md:col-span-8 xl:col-span-2",
+  3: "col-span-12 lg:col-span-12 xl:col-span-4",
+  4: "col-span-12 md:col-span-8 xl:col-span-4",
+  5: "col-span-12 md:col-span-4 xl:col-span-12",
+};
 export default function Header() {
   return (
-    <div className="flex flex-col items-center justify-center gap-16">
-      {/* Main Heading */}
-      <div className="flex flex-col items-center">
-        <div className="flex gap-2">
-          <span className={headerAnimationStyleSoft}>In the</span>
-          <span className={headerAnimationStyle}>academy today,</span>
+    <div className="flex flex-col gap-16">
+      <div className="flex grid-cols-12 flex-col items-start gap-8 xs:grid md:gap-10 lg:gap-12">
+        {/* FOUNDED IN */}
+        <div className={`col-span-12 md:col-span-4 xl:col-span-2`}>
+          <div className="flex flex-col gap-[10px]">
+            <h2 className="text-[11px] font-normal uppercase">founded in</h2>
+            <p className="text-xl font-medium text-[#0A0A0A]">2020</p>
+          </div>
         </div>
-        <div className="flex gap-2">
-          <span className={headerAnimationStyleSoft}>on the</span>
-          <span className={headerAnimationStyle}>runway tomorrow,</span>
+        {/* LOCATIONS */}
+        <div className="col-span-12 md:col-span-8 xl:col-span-2">
+          <div className="flex flex-col gap-[10px]">
+            <h2 className="text-[11px] font-normal uppercase">locations</h2>
+            <div className="flex flex-col gap-2">
+              <p className="text-xl font-medium capitalize text-[#0A0A0A]">
+                pretoria
+              </p>
+              <p className="text-xl font-medium capitalize text-[#0A0A0A]">
+                rosebank
+              </p>
+              <p className="text-xl font-medium capitalize text-[#0A0A0A]">
+                cape town
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* DESCRIPTION */}
+        <div className="col-span-12 lg:col-span-12 xl:col-span-8">
+          <div className="flex flex-col gap-4">
+            <p className="text-2xl text-[#0A0A0A]">
+              <strong>We are South Africa’s #1 modeling academy.</strong> Since
+              2020, we’ve trained 65+ models, helping them find their place in
+              the industry, from runway to pageantry, personal branding, and
+              beyond.
+            </p>
+            <p className="text-2xl font-normal text-[#0A0A0A]">
+              With a dedicated team of 7+ experienced professionals, some of
+              whom have worked with the likes of Maxhosa, David Tlale, and SAFW,
+              we’re proud to be shaping the future of modeling in Pretoria,
+              Johannesburg and Cape Town.
+            </p>
+            <p className="text-2xl font-normal text-[#0A0A0A]">
+              Whether it’s your first step on the runway or your hundredth,
+              Kalon is where you find your place.
+            </p>
+            <p className="text-2xl font-normal text-[#0A0A0A]">Welcome home.</p>
+          </div>
         </div>
       </div>
-      <div className="relative flex flex-col items-center gap-8 md:max-w-prose">
-        {/* <div className="flex flex-col items-center gap-4">
-          <p className="body-text text-center">
-            We are <strong>South Africa's #1</strong> modeling academy.{" "}
-            <strong>Since 2020</strong>, we've{" "}
-            <strong>trained 65+ models</strong>, helping them find their place
-            in the industry, from <strong>runway</strong> to{" "}
-            <strong>pageantry, personal branding,</strong> and beyond.
-          </p>
-          <p className="body-text text-center">
-            with a dedicated <strong>team of 7+</strong> experienced
-            professionals, some of whom have worked with the likes of{" "}
-            <strong>Maxhosa, David Tlale</strong> and <strong>SAFW</strong>,
-            we're proud to be shaping the future of modeling in Pretoria,
-            Johannesburg and Cape Town.
-          </p>
-        </div>
-        <div className="flex flex-col items-center gap-4">
-          <p className="body-text text-center">
-            Wether it's your first step on the runway or your hundredth, Kalon
-            is where you find your place
-          </p>
-          <p className="body-text text-center">Welcome home.</p>
-        </div> */}
-        <TextRevealByWord text="We are South Africa’s #1 modeling academy. Since 2020, we’ve trained 65+ models, helping them find their place in the industry, from runway to pageantry, personal branding, and beyond. With a dedicated team of 7+ experienced professionals, some of whom have worked with the likes of Maxhosa, David Tlale, and SAFW, we’re proud to be shaping the future of modeling in Pretoria, Johannesburg and Cape Town. Whether it’s your first step on the runway or your hundredth, Kalon is where you find your place. Welcome home." />
-      </div>
+      <Hr className="bg-gradient-to-r from-[#F8F8F8] via-[#737373] to-[#F8F8F8]" />
     </div>
   );
 }
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,17 +10,34 @@ export const metadata: Metadata = {
     "Discover Kalon Models, a top modeling academy since 2020, empowering aspiring models with expert training in runway, pageantry, and personal branding.",
 };
 
+const headerAnimationStyle = `text-3xl font-medium leading-[1.1] tracking-tight text-blackColor sm:text-3xl md:text-5xl lg:text-5xl xl:text-6xl xl:leading-[1.1] text-center`;
+const headerAnimationStyleSoft = `text-3xl font-normal leading-[1.1] tracking-tight text-blackColor sm:text-3xl md:text-5xl lg:text-5xl xl:text-6xl xl:leading-[1.1] text-center`;
+
 export default function About() {
   return (
     <>
-      <div className="relative h-[500px] w-full">
-        <Image
-          src="/kalon-cover-home.jpg"
-          alt="About Hero"
-          fill
-          className="mx-auto max-w-[1920px] object-cover saturate-0"
-          priority
-        />
+      <div className="relative">
+        <div className="relative h-[500px] w-full">
+          <div className="h-full w-full bg-[#c6c6c6]"></div>
+          {/* <Image
+            src="/kalon-cover-home.jpg"
+            alt="About Hero"
+            fill
+            className="mx-auto max-w-[1920px] bg-gray-500 object-cover brightness-0"
+            priority
+          /> */}
+        </div>
+        <h1 className="absolute left-1/2 top-1/2 flex min-w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center">
+          <span className={headerAnimationStyle}>
+            <span className={headerAnimationStyleSoft}>In the</span> academy
+            today,
+          </span>
+
+          <span className={headerAnimationStyle}>
+            <span className={headerAnimationStyleSoft}>on the</span> runway
+            tomorrow,
+          </span>
+        </h1>
       </div>
       <section
         id="about"
