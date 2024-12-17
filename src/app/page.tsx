232c5a0d65dea8546cf443ab8b55572e6014393e
StefanoCodes <stefanovidmarbusiness@gmail.com>
2024-12-17 11:35:18 +0200

    updated the hero section again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,10 +21,10 @@ export default function Home() {
                 <Diamond className="absolute left-[3px] top-[3px] z-10 h-3 w-3 md:left-[1px] md:top-[1px] md:h-4 md:w-4" />
                 <Diamond className="absolute bottom-1 left-[3px] z-10 h-3 w-3 md:bottom-[2px] md:left-[1px] md:h-4 md:w-4" />
                 <Diamond className="absolute right-[3px] top-[3px] z-10 h-3 w-3 md:right-[1px] md:top-[1px] md:h-4 md:w-4" />
-                <div className="absolute left-0 top-[8px] w-[100%] border border-[#DDDDE6] md:w-[100%]" />
-                <div className="absolute bottom-2 left-0 w-[100%] border border-[#DDDDE6] md:w-[100%]" />
-                <div className="absolute left-2 top-[0px] h-[98%] border border-[#DDDDE6] md:-top-[1px] md:h-[101%]" />
-                <div className="absolute right-2 top-[0px] h-[98%] border border-[#DDDDE6] md:-top-[1px] md:h-[101%]" />
+                <div className="absolute left-0 top-[8px] w-[100%] border border-[#E3E8EA] md:-left-[3px] md:w-[103%]" />
+                <div className="absolute bottom-2 left-0 w-[100%] border border-[#E3E8EA] md:-left-[3px] md:w-[103%]" />
+                <div className="absolute left-2 top-[0px] h-[98%] border border-[#E3E8EA] md:-top-[3px] md:h-[105%]" />
+                <div className="absolute right-2 top-[0px] h-[98%] border border-[#E3E8EA] md:-top-[3px] md:h-[105%]" />
               </span>
             </span>
             <span className="min-h-max">tomorrows models</span>
